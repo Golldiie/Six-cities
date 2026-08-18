078a82e18b4f5a976fccceb689e024cc029e3fa6
Zlata <shklyayeva.zlata@mail.ru>
2026-08-18 13:42:50 +0300

    Меняет структуру

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,5 @@
-import PlaceCard from '../place-card/place-card';
-import Header from '../header/header';
+import PlaceCard from '../../components/place-card/place-card';
+import Header from '../../components/header/header';
 
 
 type MainPageProps = {
