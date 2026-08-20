98dac1a751c72861cc7569f6db1d1265c1f10996
Zlata <shklyayeva.zlata@mail.ru>
2026-08-20 18:20:22 +0300

    Добавляет маршрутизацию

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,28 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute } from '../const';
 import MainPage from '../pages/main-page/main-page';
+import FavouritesPage from '../pages/favourites-page/favourites-page';
+import LoginPage from '../pages/login-page/login-page';
+import OfferPage from '../pages/offer-page/offer-page';
 
 function App(): JSX.Element {
   return (
-    <MainPage
-      places= {5}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root} element= {<MainPage places={5} />}
+        />
+        <Route
+          path={AppRoute.Login} element = {<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer} element = {<OfferPage />}
+        />
+        <Route
+          path={AppRoute.Favorites} element = {<FavouritesPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
